56d6c0ce9fd5c3a9f1596b7a31cec9978532c14f
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2020-07-08 14:21:06 -0500

    Metodo para encripttar el nombre del archivo

---
+++ src/main/java/ec/edu/ups/controlador/ControladorArchivos.java
@@ -0,0 +1,32 @@
+package ec.edu.ups.controlador;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class ControladorArchivos {
+
+    private String ruta;
+    private String archivos;
+    
+    private Map<String, String> hola;
+    
+    public ControladorArchivos() {
+        
+    }
+    
+    public String encriptarNombreArchivo(String nombre){
+        
+        
+        
+        return null;
+    }
+    
+    public void llenarDiccionario(){
+        String[] letras = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+        "o","p","q","r","s","t","u","v","w","x","y","z"};
+        String[] encriptado = {"","8",};
+    }
+}
