b8faf1d2694bb3592867629daffe2c89199dab35
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2020-07-10 21:53:54 -0500

    Documentacion de la clase ControladorArchivos

--- src/main/java/ec/edu/ups/controlador/ControladorArchivos.java
+++ src/main/java/ec/edu/ups/controlador/ControladorArchivos.java
@@ -9,21 +9,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
- * @author Estudiantes
+ * Clase Controlador Archivos.
+ * 
+ * Esta clase permite encriptar el mensaje ingresado ademas crea un archivo en 
+ * el cual es guardado en cualquier ruta que decida el usuario.
+ * @author Romel Avila, Sebastian Uyaguari
  */
 public class ControladorArchivos {
 
     private String ruta;
-    private String archivos;
+    private File archivo;
     
     private Map<String, String> diccionarioEncriptado;
     
-   public ControladorArchivos() {
+    /**
+    * Metodo ControladorArchivos (Constructor).
+    * El metodo constructor inicializa el atributo diccionarioEncriptado, ademas 
+    * llena el diccionario con los datos correspondientes para poder encriptar.
+    */
+    public ControladorArchivos() {
         diccionarioEncriptado = new HashMap<>();
         llenarDiccionario();
     }
     
+    /**
+     * Metodo encriptarMensajeArchivo.
+     * El metodo encripta el mensaje recibido como parametro cambiando las letras
+     * por un caracter especifico para despues retornar el 
+     * @param mensaje
+     * @return retorna una variable tipo String: textoEncriptado
+     */    
     public String encriptarMensajeArchivo(String mensaje){
       
         String textoEncriptado="";
@@ -41,12 +56,16 @@ public class ControladorArchivos {
             }else{
                 textoEncriptado+="-";
             } 
-        }
-
-        
+        } 
         return textoEncriptado;
     }
     
+    /**
+     * Metodo llenarDiccionario.
+     * Este metodo tiene la funcion de llenar el diccionario mediante dos vectores,
+     * el cual el primero es para asignar la clave o llave mientras que el segundo
+     * es para asignar el velor.
+     */
     public void llenarDiccionario(){
         String[] letras = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n",
         "o","p","q","r","s","t","u","v","w","x","y","z"," "};
@@ -58,9 +77,18 @@ public class ControladorArchivos {
         
     }
     
+    /**
+     * Metodo crearArchivoDeTexto.
+     * El metodo realiza la creacion del archivo con la informacion que es 
+     * recibida mediante parametros y es colocada en un solo archivo para ser 
+     * guardado en un solo archivo.
+     * @param rutas
+     * @param nombre
+     * @param texto 
+     */
     public void crearArchivoDeTexto(String rutas,String nombre, String texto){
         try{
-            File archivo = new File(rutas + File.separator + nombre);
+            archivo = new File(rutas + File.separator + nombre);
             String rutaCompleta=archivo.getAbsolutePath();
             
             System.out.println(rutaCompleta);
