11399b4d1234fae4a6aafa827c328db5ce3e29d7
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2020-07-11 16:10:04 -0500

    Correccion error

--- src/main/java/ec/edu/ups/controlador/ControladorArchivos.java
+++ src/main/java/ec/edu/ups/controlador/ControladorArchivos.java
@@ -69,7 +69,7 @@ public class ControladorArchivos {
     public void llenarDiccionario(){
         String[] letras = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n",
         "o","p","q","r","s","t","u","v","w","x","y","z"," "};
-        String[] encriptado = {"!","]","^","æ","ü","×","¢","þ","≡","§","«",
+        String[] encriptado = {"!","]","^","æ","ü","×","¢","þ","@","§","«",
             "A","¥","~","c","r","z","W","8","ç","2","L","f","&","#","[","-"};
         for (int i = 0; i < 26; i++) {
             diccionarioEncriptado.put(letras[i], encriptado[i]);
